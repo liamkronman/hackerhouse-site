4b51e16350cf15d8d80216127688dc24dd6a1049
liamkronman <liamkronman@gmail.com>
2022-05-30 00:56:16 -0400

    two more members added

--- hackerhouse-site/pages/index.js
+++ hackerhouse-site/pages/index.js
@@ -51,21 +51,18 @@ export default function Home() {
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2>Liam Kronman</h2>
-            <p>built a <a href="https://www.insider.com/mit-student-made-fake-dating-app-prank-harvard-students-2021-11" className={styles.generalLink}>prank dating site</a> for Harvard (got quarter of school to sign up), created and run <a href="https://exexex.io" className={styles.generalLink}>ExExEx.io</a> (dating site that matches you with exes’ exes’ exes), made <a href="https://superlatives.app" className={styles.generalLink}>Superlatives</a> (voting game for friends) and <a href="https://beatstreet.live" className={styles.generalLink}>BeatStreet</a> (GitHub for original music)</p>
+            <p>Built a <a href="https://www.insider.com/mit-student-made-fake-dating-app-prank-harvard-students-2021-11" className={styles.generalLink}>prank dating site</a> for Harvard (got quarter of school to sign up), created and run <a href="https://exexex.io" className={styles.generalLink}>ExExEx.io</a> (dating site that matches you with exes’ exes’ exes), made <a href="https://superlatives.app" className={styles.generalLink}>Superlatives</a> (voting game for friends) and <a href="https://beatstreet.live" className={styles.generalLink}>BeatStreet</a> (GitHub for original music)</p>
           </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.card}>
+            <h2>Sofiane Larbi</h2>
+            <p>Making <a href="https://superlatives.app" className={styles.generalLink}>Superlatives.app</a> (app for generating superlatives for friends), made BuddyList (AIM style messenger), NFT arbitrage bot, DMV reservations bot, Katapult (website for buying and selling physical ad space), Reso (app for waiting in line remotely), rocket engine design algorithm</p>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <div className={styles.card}>
+            <h2>William Lin</h2>
+            <p>Focused on competitive programming in high school and won <a href="https://en.wikipedia.org/wiki/International_Olympiad_in_Informatics" className={styles.generalLink}>IOI</a> 2020. Created and grew a <a href="https://www.youtube.com/c/WilliamLin168" className={styles.generalLink}>YouTube channel</a> to 100k subscribers in just 6 months. Building <a href="https://nyalabs.com/" className={styles.generalLink}>NyaLabs</a>, a platform which helps influencers easily create NFTs and helps their audiences easily buy NFTs.</p>
+          </div>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -77,6 +74,13 @@ export default function Home() {
             </p>
           </a>
         </div>
+
+        <h3 className={styles.header}>PROJECTS</h3>
+        <div className={styles.projects}>
+          Coming soon...
+        </div>
+
+        If you'd like to help support HackerHouse, please contact us at <a href="mailto: "></a>!
       </main>
 
       <footer className={styles.footer}>
